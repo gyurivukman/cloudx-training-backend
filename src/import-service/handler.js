c2493b9c2deea55716004963d188c9450c2f4169
György Vukman <gyurivukman@gmail.com>
2023-10-29 18:03:29 +0100

    Task 6 prototype done

--- src/import-service/handler.js
+++ src/import-service/handler.js
@@ -5,12 +5,16 @@ const {
   PutObjectCommand,
   GetObjectCommand,
 } = require("@aws-sdk/client-s3");
+
+const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
+
 const csv = require("csv-parser");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const FILE_QUERY_PARAM = process.env.FILE_NAME_PARAMETER;
 const IMPORT_BUCKET_NAME = process.env.IMPORT_BUCKET_NAME;
 const client = new S3Client({ region: "eu-central-1" });
+const sqsClient = new SQSClient({ region: "eu-central-1" });
 
 module.exports.getSignedProductImportUrl = async function (
   event,
@@ -29,7 +33,7 @@ module.exports.getSignedProductImportUrl = async function (
 };
 
 module.exports.parseProductCsv = async function (event, context, callback) {
-  const Key = event.Records[0].s3.object.key; //Can only handle 1 upload, too lazy to write a for loop and print all. Maybe even safer for my wallet this way with the cloudwatch logging!
+  const Key = event.Records[0].s3.object.key; //Can only handle 1 file upload, too lazy to write a for loop and print all. Maybe even safer for my wallet this way with the cloudwatch logging!
 
   const command = new GetObjectCommand({
     Bucket: IMPORT_BUCKET_NAME,
@@ -39,9 +43,16 @@ module.exports.parseProductCsv = async function (event, context, callback) {
   const file = await client.send(command);
   const parser = csv();
 
-  parser.on('data', (data) => {
-    console.log(data);
+  parser.on("data", async (product) => {
+    const cmd = new SendMessageCommand({
+      QueueUrl:
+        "https://sqs.eu-central-1.amazonaws.com/135024919331/product-service-dev-catalogItemsQueue-k5LMSXnz6PsP",
+      MessageBody: JSON.stringify(product),
+    });
+
+    await sqsClient.send(cmd);
   });
+
   file.Body.pipe(parser);
 
   callback(null, 201);
